e7f28c5b9e534f0e234da314627a4962a0884abf
Chase090 <carloyam21@icloud.com>
2021-09-24 01:19:33 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { actors } from '../data';
 
+const renderActors = () => {
+  return actors.map((actor, index) => (
+    <div key={index}>
+      <h3>{actor.name}</h3>
+      <h4>Movies</h4>
+        <ul>
+          {actor.movies.map((movie, index) => (
+            <li key={index}>{movie}</li>
+          ))}
+        </ul>
+    </div>
+  ))
+}
+
 const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { directors } from '../data';
 
+const renderDirectors = () => {
+  return directors.map((director, index) => (
+    <div key={index}>
+      <h3>{director.name}</h3>
+      <h4>Movies</h4>
+        <ul>
+          {director.movies.map((movie, index) => (
+            <li key={index}>{movie}</li>
+          ))}
+        </ul>
+    </div>
+  ))
+}
+
 const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {/* {console.log(directors)} */}
+      {renderDirectors()}
     </div>
   );
 }
